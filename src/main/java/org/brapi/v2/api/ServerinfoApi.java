0c0a57f2f91e6cc8bf9d22ab516003e7eb761542
sempere <guilhem.sempere@cirad.fr>
2021-01-26 15:43:58 +0100

    Changed serverInfo's tag from "Calls" to "Server info"

--- src/main/java/org/brapi/v2/api/ServerinfoApi.java
+++ src/main/java/org/brapi/v2/api/ServerinfoApi.java
@@ -32,7 +32,7 @@ public interface ServerinfoApi {
 	String URL_BASE_PREFIX = "/brapi/v2";
 
     @ApiOperation(value = "Get the list of implemented Calls", nickname = "serverinfoGet", notes = "Implementation Notes  Having a consistent structure for the path string of each call is very  important for teams to be able to connect and find errors. Read more on Github.  Here are the rules for the path of each call that should be returned  Every word in the call path should match the documentation exactly, both in  spelling and capitalization. Note that path strings are all lower case, but  path parameters are camel case.  Each path should start relative to \\\"/\\\" and therefore should not include \\\"/\\\"  No leading or trailing slashes (\\\"/\\\")   Path parameters are wrapped in curly braces (\\\"{}\\\"). The name of the path parameter  should be spelled exactly as it is specified in the documentation.  Examples   GOOD   \"call\": \"germplasm/{germplasmDbId}/pedigree\"   BAD    \"call\": \"germplasm/{id}/pedigree\"  BAD    \"call\": \"germplasm/{germplasmdbid}/pedigree\"   BAD    \"call\": \"brapi/v2/germplasm/{germplasmDbId}/pedigree\"   BAD    \"call\": \"/germplasm/{germplasmDbId}/pedigree/\"   BAD    \"call\": \"germplasm/<germplasmDbId>/pedigree\"", response = CallsResponse.class, authorizations = {
-        @Authorization(value = "AuthorizationToken")    }, tags={ "Calls", })
+        @Authorization(value = "AuthorizationToken")    }, tags={ "Server info", })
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "OK", response = CallsResponse.class),
         @ApiResponse(code = 400, message = "Bad Request", response = String.class),
