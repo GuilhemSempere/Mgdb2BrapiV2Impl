87be3a8445da8d7e19780078f4954f244659fefc
Alice Boizet <alice.boizet@cirad.fr>
2024-10-10 11:17:42 +0200

    fix getting commonCropNames from hidden databases and fix filtering trials on commonCropNames

--- src/main/java/org/brapi/v2/api/CommoncropnamesApiController.java
+++ src/main/java/org/brapi/v2/api/CommoncropnamesApiController.java
@@ -43,39 +43,35 @@ public class CommoncropnamesApiController implements CommoncropnamesApi {
         this.request = request;
     }
 
+    @Override
     public ResponseEntity<CommonCropNamesResponse> commoncropnamesGet(@ApiParam(value = "Which result page is requested. The page indexing starts at 0 (the first page is 'page'= 0). Default is `0`.") @Valid @RequestParam(value = "page", required = false) Integer page,@ApiParam(value = "The size of the pages to be returned. Default is `1000`.") @Valid @RequestParam(value = "pageSize", required = false) Integer pageSize,@ApiParam(value = "HTTP HEADER - Token used for Authorization   <strong> Bearer {token_string} </strong>" ) @RequestHeader(value="Authorization", required=false) String authorization) {
-		String token = ServerinfoApiController.readToken(authorization);
+        String token = ServerinfoApiController.readToken(authorization);
     	
         try {
-        	CommonCropNamesResponse rlr = new CommonCropNamesResponse();
-        	CommonCropNamesResponseResult result = new CommonCropNamesResponseResult();
-        	if (page == null)
-        		page = 0;
-        	int nAllowedCropIndex = 0;
-        	for (final String crop : MongoTemplateManager.getAvailableModules().stream()
-    	        .filter(db -> {
-					try {
-						return tokenManager.canUserReadDB(token, db);
-					} catch (ObjectNotFoundException e) {
-						return false;
-					}
-				}).map(db -> MongoTemplateManager.getTaxonName(db)).collect(Collectors.toSet()))
-        	{
-    			if (pageSize == null || (page*pageSize <= nAllowedCropIndex && (page+1)*pageSize > nAllowedCropIndex))
-	        		result.addDataItem(crop == null ? "" : crop);
-    			nAllowedCropIndex++;
-    		}
+            CommonCropNamesResponse rlr = new CommonCropNamesResponse();
+            CommonCropNamesResponseResult result = new CommonCropNamesResponseResult();
+            if (page == null)
+                    page = 0;
+            int nAllowedCropIndex = 0;
+            for (final String crop : tokenManager.listReadableDBs(token).stream()
+                .map(db -> MongoTemplateManager.getTaxonName(db)).collect(Collectors.toSet())) {                
+                if (crop != null) {
+                    if (pageSize == null || (page*pageSize <= nAllowedCropIndex && (page+1)*pageSize > nAllowedCropIndex))
+                        result.addDataItem(crop);
+                    nAllowedCropIndex++;
+                }
+            }
 
-			Metadata metadata = new Metadata();
-			IndexPagination pagination = new IndexPagination();
-			pagination.setPageSize(result.getData().size());
-			pagination.setCurrentPage(0);
-			pagination.setTotalPages(1);
-			pagination.setTotalCount((long) result.getData().size());
-			metadata.setPagination(pagination);
-			rlr.setMetadata(metadata);
-			
-			rlr.setResult(result);
+            Metadata metadata = new Metadata();
+            IndexPagination pagination = new IndexPagination();
+            pagination.setPageSize(result.getData().size());
+            pagination.setCurrentPage(0);
+            pagination.setTotalPages(1);
+            pagination.setTotalCount((long) result.getData().size());
+            metadata.setPagination(pagination);
+            rlr.setMetadata(metadata);
+
+            rlr.setResult(result);
             return new ResponseEntity<CommonCropNamesResponse>(rlr, HttpStatus.OK);
         } catch (Exception e) {
             log.error("Couldn't serialize response for content type application/json", e);
